3158930834b0197dea215d6cddeb6781c9a06763
Param Singh <techhero724@gmail.com>
2026-02-22 13:17:08 +0530

    Fix allowedDevOrigins for nextjs 16

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: [
+    "http://192.168.1.2:3000",
+    "http://192.168.1.2:3001",
+    "http://localhost:3000",
+  ]
 };
 
 export default nextConfig;
